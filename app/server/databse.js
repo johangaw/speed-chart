f853995b6c853ecc6ae06dcc6fc99693579b36fc
Johan Gustavsson <johan.gustavsson@valtech.se>
2018-04-04 22:24:24 +0200

    Further improved error handling database connection

--- app/server/databse.js
+++ app/server/databse.js
@@ -4,10 +4,10 @@ const CONFIG = require('../config/node-config').DATABASE
 function insertDatapoint (timeStamp, upSpeed, downSpeed) {
   return new Promise((resolve, reject) => {
     MongoClient.connect(CONFIG.URL, (err, client) => {
-      if (err) return reject(new Error('Could not connect to database: ' + err))
+      if (err) return reject(new Error(`${err.name}: ${err.message}`))
       const collection = client.db(CONFIG.NAME).collection(CONFIG.COLLECTIONS.DATAPOINTS)
       collection.insert({timeStamp, upSpeed, downSpeed}, (err, result) => {
-        if (err) return reject(new Error('Could not insert: ' + err))
+        if (err) return reject(new Error(`${err.name}: ${err.message}`))
         client.close()
         resolve()
       })
@@ -18,10 +18,10 @@ function insertDatapoint (timeStamp, upSpeed, downSpeed) {
 function getAllDatapoints () {
   return new Promise((resolve, reject) => {
     MongoClient.connect(CONFIG.URL, (err, client) => {
-      if (err) return reject(new Error('Could not connect to database: ' + err))
+      if (err) return reject(new Error(`${err.name}: ${err.message}`))
       const collection = client.db(CONFIG.NAME).collection(CONFIG.COLLECTIONS.DATAPOINTS)
       collection.find({}).toArray((err, items) => {
-        if (err) return reject(new Error('Could not get all: ' + err))
+        if (err) return reject(new Error(`${err.name}: ${err.message}`))
         client.close()
         resolve(items)
       })
