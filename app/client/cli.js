21c9fbfb36791c315fc8573746a34123c82081fb
Johan Gustavsson <johan.gustavsson@valtech.se>
2018-04-28 16:48:03 +0200

    Impreoved client logging

--- app/client/cli.js
+++ app/client/cli.js
@@ -3,27 +3,30 @@ const path = require('path')
 const speedTest = require('speedtest-net')
 const minimist = require('minimist')
 const sendData = require('./upload').sendData
-const TIMES = require('../config/node-config').TIMES
+const {TIMES} = require('../config/node-config')
 
 const argv = minimist(process.argv.slice(2), {default: {intervall: 1}})
 const min2ms = (minute) => minute * 60 * 1000
+const key = argv.key
 
-runTest()
-setInterval(runTest, min2ms(argv.intervall), argv.key)
+runTest(key)
+setInterval(runTest, min2ms(argv.intervall), key)
 
 function runTest (key) {
-  console.log('running...')
+  console.log('test started...')
   return new Promise((resolve, reject) => {
     const test = speedTest({ maxTime: TIMES.FIVE_SECONDS })
     test.on('data', data => {
       const timeStamp = new Date()
-      resolve(saveData(timeStamp, data.speeds.upload, data.speeds.download))
-      sendData(key, timeStamp, data.speeds.upload, data.speeds.download)
+      Promise.all([
+        saveData(timeStamp, data.speeds.upload, data.speeds.download),
+        sendData(key, timeStamp, data.speeds.upload, data.speeds.download)
+      ]).then((results) => resolve(results))
     })
     test.on('error', err => {
       reject(err)
     })
-  })
+  }).then((data) => console.log('success: ' + data), (err) => console.log('error: ' + err))
 }
 
 function saveData (timeStamp, upSpeed, downSpeed) {
