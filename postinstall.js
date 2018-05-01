7afb64c2887e089f6b5362098054344d3fb7df83
Johan Gustavsson <johan.gustavsson@valtech.se>
2018-05-01 16:48:54 +0200

    Replaced postinstall with prepublish

--- postinstall.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process')
-
-console.log('Running postinstall.js...')
-if (process.env.NODE_ENV) {
-  const output = execSync('npm run build')
-  console.log(output.toString('utf8'))
-}
